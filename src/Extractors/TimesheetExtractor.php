794d85829abd5a071b570d9dceda903645ed19d3
[dyad] <clesio@sharksolucoes.com.br>
2026-03-08 19:06:30 -0300

    [dyad] Alteração da data de lançamento para o campo 'date' no ETL de Timesheet. - wrote 1 file(s)

--- src/Extractors/TimesheetExtractor.php
+++ src/Extractors/TimesheetExtractor.php
@@ -38,7 +38,7 @@ final class TimesheetExtractor {
           e.name as cliente,
           COALESCE(g.name, 'Sem Grupo') as grupo_solucionador,
           COALESCE(NULLIF(TRIM(CONCAT(IFNULL(u.firstname,''),' ',IFNULL(u.realname,''))),''), u.name) as tecnico,
-          tk.begin as data_lancamento,
+          tk.date as data_lancamento,
           (tk.actiontime / 3600) as horas,
           COALESCE(tc.name, 'Comercial') as tipo_hora,
           UTC_TIMESTAMP() as data_carga
