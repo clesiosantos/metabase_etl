fa1d575dc16730a56765b7def1ca5663f0d3dc84
[dyad] <clesio@sharksolucoes.com.br>
2026-03-18 14:18:18 -0300

    [dyad] Proposta de debug avançado para localizar dados de timesheet em campos customizados (Plugin Fields) do GLPI. - wrote 1 file(s)

--- sql/debug_ticket_64639.sql
+++ sql/debug_ticket_64639.sql
@@ -1,16 +1,25 @@
--- Execute este comando no banco de dados do GLPI (Origem)
--- Objetivo: Validar se os campos 'begin' e 'date' estão preenchidos na origem
+-- 1. Primeiro, vamos ver o que tem na tarefa padrão para o ticket 64639
+SELECT 
+    id, 
+    tickets_id, 
+    `date` AS data_criacao_padrao, 
+    `begin` AS data_inicio_padrao, 
+    actiontime 
+FROM glpi_tickettasks 
+WHERE tickets_id = 64639;
 
-SELECT
-    tk.id AS id_tarefa_glpi,
-    p.id AS id_ticket,
-    tk.begin AS data_inicio_real, -- Mapeado para data_lancamento
-    tk.date AS data_registro,     -- Mapeado para data_criacao_tarefa
-    tk.actiontime AS tempo_segundos,
-    u.name AS tecnico_login,
-    tc.name AS categoria_tarefa
+-- 2. Agora, vamos procurar se existem tabelas de 'Plugin Fields' vinculadas às tarefas.
+-- Execute este comando para listar tabelas que podem conter os dados do seu "formulário"
+SHOW TABLES LIKE 'glpi_plugin_fields_tickettask%';
+
+-- 3. Se você souber o nome da tabela do formulário (ex: glpi_plugin_fields_tickettasktimesheets), 
+-- tente rodar o join abaixo (ajuste o nome da tabela 'f' se for diferente):
+/*
+SELECT 
+    tk.id AS tarefa_id,
+    tk.tickets_id,
+    f.* -- Aqui veremos todos os campos do seu formulário
 FROM glpi_tickettasks tk
-JOIN glpi_tickets p ON p.id = tk.tickets_id
-LEFT JOIN glpi_users u ON u.id = tk.users_id
-LEFT JOIN glpi_taskcategories tc ON tc.id = tk.taskcategories_id
-WHERE p.id = 64639;
+LEFT JOIN glpi_plugin_fields_tickettaskXXXXXXXXs f ON f.items_id = tk.id
+WHERE tk.tickets_id = 64639;
+*/
