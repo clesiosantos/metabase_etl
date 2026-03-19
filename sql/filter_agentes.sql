9768992bdd6c8cd1f26f18d0cfecd2a283b5a1cc
[dyad] <clesio@sharksolucoes.com.br>
2026-03-19 14:25:36 -0300

    [dyad] Corrigido erro de collation no SQL do filtro unificado de agentes. - wrote 1 file(s)

--- sql/filter_agentes.sql
+++ sql/filter_agentes.sql
@@ -1,27 +1,27 @@
 -- Consulta para Filtro Unificado de Agentes (Metabase)
--- Consolida nomes de metabase_tickets, metabase_changes, metabase_problems e metabase_timesheet
+-- Resolvido: Illegal mix of collations (Error 1271)
 
 SELECT DISTINCT agente
 FROM (
-    SELECT agente_solucionador AS agente 
+    SELECT agente_solucionador COLLATE utf8mb4_unicode_ci AS agente 
     FROM metabase_tickets 
     WHERE agente_solucionador IS NOT NULL AND agente_solucionador <> ''
 
     UNION
 
-    SELECT agente_solucionador AS agente 
+    SELECT agente_solucionador COLLATE utf8mb4_unicode_ci AS agente 
     FROM metabase_changes 
     WHERE agente_solucionador IS NOT NULL AND agente_solucionador <> ''
 
     UNION
 
-    SELECT agente_solucionador AS agente 
+    SELECT agente_solucionador COLLATE utf8mb4_unicode_ci AS agente 
     FROM metabase_problems 
     WHERE agente_solucionador IS NOT NULL AND agente_solucionador <> ''
 
     UNION
 
-    SELECT tecnico AS agente 
+    SELECT tecnico COLLATE utf8mb4_unicode_ci AS agente 
     FROM metabase_timesheet 
     WHERE tecnico IS NOT NULL AND tecnico <> ''
 ) AS consolidado
